b2c7126e1e290939969209bf073e423da70eca42
deploy <baysaa41@yahoo.com>
2025-01-06 21:26:15 +0800

    remote .env remove

--- mathminds/settings.py
+++ mathminds/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost','mathminds.local']
+ALLOWED_HOSTS = ['localhost','mathminds.club','api.mathminds.club','www.mathminds.club']
 
 
 # Application definition
